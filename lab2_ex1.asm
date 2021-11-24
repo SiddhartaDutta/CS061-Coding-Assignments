1cbea3d89cb8bec3251d1c371f7f128ca11df885
Siddharta Dutta <52301934+SiddhartaDutta@users.noreply.github.com>
2021-11-23 22:16:07 -0800

    Add files via upload

---
+++ lab2_ex1.asm
@@ -0,0 +1,25 @@
+;=================================================
+; Name: Siddharta Dutta
+; Email: sdutt017@ucr.edu
+; 
+; Lab: lab 2. ex 1
+; Lab section: 021
+; TA: Shirin Shirazi
+; 
+;=================================================
+
+.ORIG x3000
+
+; Program Code
+    
+    LD R3, DEC_65
+    LD R4, HEX_41
+    
+    HALT
+    
+; Local Data
+
+    DEC_65 .FILL #65
+    HEX_41 .FILL x41
+
+.END
